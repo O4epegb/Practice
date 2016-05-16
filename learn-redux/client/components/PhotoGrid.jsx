722461d81125ec0a09b498b90e886f85be208b0b
Danila <O4epegb@mail.ru>
2016-05-16 23:50:19 +0400

    wesbos learn redux

---
+++ learn-redux/client/components/PhotoGrid.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PhotoGrid extends React.Component {
+  render() {
+    return (
+      <div className="photo-grid">
+        grid
+      </div>
+    );
+  }
+}
+
+export default PhotoGrid;
---
+++ learn-redux/client/reduxstagram.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import css from './styles/style.styl';
+
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+);
+
+render(router, document.getElementById('root'));
