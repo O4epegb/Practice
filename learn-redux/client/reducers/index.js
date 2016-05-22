1d967f240e3619a5880688711a9669e8120b8259
Danila <O4epegb@mail.ru>
2016-05-22 23:58:16 +0400

    react-redux added

--- learn-redux/client/reducers/index.js
+++ learn-redux/client/reducers/index.js
@@ -5,6 +5,6 @@ import posts from './posts';
 import comments from './comments';
 
 
-const rootReducer = combineReducers({posts, comments, router: routerReducer});
+const rootReducer = combineReducers({posts, comments, routing: routerReducer});
 
 export default rootReducer;
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
+import { Provider } from 'react-redux';
 
 import css from './styles/style.styl';
 
 import Main from './components/Main';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
+import store, { history } from './store'
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
 );
 
 render(router, document.getElementById('root'));
