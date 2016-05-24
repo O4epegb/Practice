8528fce3ca6fd7531e364fea5b6123b7988da31d
Danila <O4epegb@mail.ru>
2016-05-24 23:56:12 +0400

    posts reducer and comment added

--- learn-redux/client/components/Single.jsx
+++ learn-redux/client/components/Single.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 class Single extends React.Component {
   render() {
+    const i = this.props.posts.findIndex(post => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
+
     return (
       <div className="single-photo">
-        single
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
       </div>
     );
   }
--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -1,6 +1,15 @@
 function posts(state = [], action) {
-  console.log(state, action);
-  return state;
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      const i = action.index;
+      return [
+        ...state.slice(0, i),
+        {...state[i], likes: ++state[i].likes },
+        ...state.slice(i + 1)
+      ];
+    default:
+      return state
+  }
 }
 
 export default posts;
