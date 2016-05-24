c9b0c1afed67d8bef1c4912add8be54055d43cc0
Danila <O4epegb@mail.ru>
2016-05-25 01:18:45 +0400

    comments added

--- learn-redux/client/components/Comments.jsx
+++ learn-redux/client/components/Comments.jsx
@@ -2,10 +2,37 @@ import React from 'react';
 
 
 class Comments extends React.Component {
-  render() {    
+  renderComment(comment, i) {
     return (
-      <div className="comment">
-        I'm a comment!
+      <div className="comment" key={i}>
+        <strong>{comment.user}</strong>
+        {comment.text}
+        <button className="remove-comment">&times;</button>
+      </div>
+    );
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const postId = this.props.params.postId;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    this.props.addComment(postId, author, comment);
+  }
+
+  render() {
+
+    return (
+      <div className="comments">
+        {this.props.postComments.map(this.renderComment)}
+        <form
+          ref="commentForm"
+          className="comment-form"
+          onSubmit={this.handleSubmit}>
+          <input type="text" ref="author" placeholder="author" />
+          <input type="text" ref="comment" placeholder="comment" />
+          <input type="submit" hidden />
+        </form>
       </div>
     );
   }
--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,6 +1,16 @@
 function comments(state = [], action) {
-  console.log(state, action);
-  return state;
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      // const i = action.index;
+      // return [
+      //   ...state.slice(0, i),
+      //   {...state[i], likes: ++state[i].likes },
+      //   ...state.slice(i + 1)
+      // ];
+      return state
+    default:
+      return state
+  }
 }
 
 export default comments;
