c59c8c9a0f53d9743b28ccf0bfc14ea9406fc9cb
Danila <O4epegb@mail.ru>
2016-05-31 01:01:57 +0400

    trying simple todo app

--- MobxTest/src/index.jsx
+++ MobxTest/src/index.jsx
@@ -1,48 +1,71 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import {observable} from 'mobx';
+import {observable, action, useStrict} from 'mobx';
 import {observer} from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 
-// const appState =  new class AppState {
-//     @observable timer = 0;
-//
-//     constructor() {
-//         setInterval(() => {
-//             appState.timer += 1;
-//         }, 1000);
-//     }
-//
-//     resetTimer() {
-//         this.timer = 0;
-//     }
-// }();
+useStrict(true);
 
-// @observer
+const store = observable({
+    todos: [
+        {
+            name: 'keke',
+            done: false
+        }
+    ]
+})
+
+const addTodo = action(function(store, todo) {
+    store.todos.push(todo);
+})
+
+const toggleTodo = action(function(store, targetTodo) {
+    store.todos = store.todos.map(todo => {
+      if (targetTodo === todo) {
+        todo.done = !todo.done;
+      }
+      return todo;
+    })
+})
+
+@observer
 class TimerView extends Component {
+    state = {
+        text: ''
+    }
+
     render() {
         return (
             <div>
-                olol
-                <span>asdas</span>
+                <button onClick={this.addTodo}>
+                    add
+                </button>
+                <input type="text" value={this.state.text} onChange={(e) => {
+                    this.setState({text: e.target.value})
+                }}/>
+                {this.props.store.todos.map(todo =>
+                  <div onClick={() => this.toggleTodo(todo)}
+                       style={{'text-decoration': todo.done ? 'line-through' : 'none'}}>
+                    {todo.name}
+                  </div>
+                )}
+                <DevTools/>
             </div>
         );
     }
-    //  render() {
-    //     return (
-    //         <div>
-    //             <button onClick={this.onReset}>
-    //                 Seconds passed: {this.props.appState.timer}
-    //             </button>
-    //             <DevTools />
-    //         </div>
-    //     );
-    //  }
 
-    //  onReset = () => {
-    //  	this.props.appState.resetTimer();
-    //  }
+    toggleTodo = (todo) => {
+      toggleTodo(this.props.store, todo);
+    }
+
+    addTodo = () => {
+        addTodo(this.props.store, {
+            name: this.state.text || 'untitled',
+            done: false
+        });
+        this.setState({text: ''})
+    }
 };
 
 ReactDOM.render(
-    <TimerView appState={appState}/>, document.getElementById('root'));
+    <TimerView store={store}/>, document.getElementById('root'));
