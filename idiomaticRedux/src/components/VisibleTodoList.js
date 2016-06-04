2fc43de0221bf3c69848f274c8ab0bd23d017535
Danila <O4epegb@mail.ru>
2016-06-04 20:02:27 +0400

    minor refactor

--- idiomaticRedux/src/components/VisibleTodoList.js
+++ idiomaticRedux/src/components/VisibleTodoList.js
@@ -1,33 +1,26 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import { toggleTodo } from '../actions';
+import { getVisibleTodos } from './reducers';
 import TodoList from './TodoList';
 
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case 'SHOW_ALL':
-      return todos;
-    case 'SHOW_COMPLETED':
-      return todos.filter(t => t.completed);
-    case 'SHOW_ACTIVE':
-      return todos.filter(t => !t.completed);
-    default:
-      throw new Error(`Unknown filter: ${filter}.`);
-  }
-};
 
-const mapStateToProps = (state) => ({
-  todos: getVisibleTodos(state.todos, state.visibilityFilter),
+const mapStateToProps = (state, { params }) => ({
+  todos: getVisibleTodos(
+    state,
+    params.filter || 'all'
+  ),
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  onTodoClick(id) {
-    dispatch(toggleTodo(id));
-  },
-});
+// const mapDispatchToProps = (dispatch) => ({
+//   onTodoClick(id) {
+//     dispatch(toggleTodo(id));
+//   },
+// });
 
-const VisibleTodoList = connect(
+const VisibleTodoList = withRouter(connect(
   mapStateToProps,
-  mapDispatchToProps
-)(TodoList);
+  { onTodoClick: toggleTodo }
+)(TodoList));
 
 export default VisibleTodoList;
