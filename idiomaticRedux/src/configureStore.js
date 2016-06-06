19a46b12369eee3056406e81cf34357be240fbea
Danila <O4epegb@mail.ru>
2016-06-06 17:44:14 +0400

    reducers and middleware refactoring

--- idiomaticRedux/src/configureStore.js
+++ idiomaticRedux/src/configureStore.js
@@ -1,45 +1,20 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import todoApp from './reducers';
 
 
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  if (!console.group) {
-    return rawDispatch;
-  }
-
-  return (action) => {
-    console.group(action.type);
-    console.log('%c prev state', 'color:gray', store.getState());
-    console.log('%c action', 'color:blue', action);
-    const returnValue = rawDispatch(action);
-    console.log('%c next state', 'color:green', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  };
-};
-
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(rawDispatch);
-    }
-    return rawDispatch(action);
-  };
-};
-
 function configureStore() {
-  const store = createStore(todoApp);
+  const middlewares = [promise];
 
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(createLogger());
   }
 
-  store.dispatch = addPromiseSupportToDispatch(store);
-
-  return store;
+  return createStore(
+    todoApp,
+    applyMiddleware(...middlewares)
+  );
 };
 
 export default configureStore;
