68b7e09442e0c168a8727f6606bdd52274399279
Danila <O4epegb@mail.ru>
2016-08-21 23:47:19 +0400

    fun with electron

---
+++ electronWriteAndDeleteFiles/src/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+
+export default class App extends React.Component<{}, {}> {
+    render() {
+        return (
+            <div>
+                ololo
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ electronWriteAndDeleteFiles/webpack.config.js
@@ -0,0 +1,49 @@
+var path = require("path");
+var webpack = require('webpack');
+
+var config = {
+    entry: {
+      "app": "./src/index.tsx"
+    },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "[name].js"
+    },
+    resolve: {
+        extensions: ["", ".ts", ".tsx", ".js", ".jsx", ".css", ".styl"]
+    },
+    plugins: [],
+    module: {
+        loaders: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/
+            }, {
+                test: /\.css$/,
+                loaders: ['style', 'css']
+            }, {
+                test: /\.styl$/,
+                loaders: ['style', 'css', 'stylus']
+            }
+        ]
+    }
+};
+
+if (process.env.NODE_ENV === 'production') {
+    config.plugins = [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            }
+        })
+    ]
+}
+
+module.exports = config;
