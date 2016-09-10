1e64ab6d05a08321df5c94c315cde1eea8a58a75
Danila <O4epegb@mail.ru>
2016-09-10 17:03:35 +0400

    tweak jest coverage and watcher

--- algorithms-unlocked/in-typescript/src/quicksort-sort/recursive-quick-sort.ts
+++ algorithms-unlocked/in-typescript/src/quicksort-sort/recursive-quick-sort.ts
@@ -1,14 +1,14 @@
-// export default function quickSort(array, left, right) {
-//   if (array.length === 1) {
-//     return;
-//   } else {
-//     const anchor = array[array.length - 1];
-//     const anchorPoint = partition(array, left, right);
-//     quickSort(array, left, anchorPoint - 1);
-//     quickSort(array, anchorPoint + 1, right);
-//   }
-// }
-//
-// function partition(array, left, right) {
-//
-// }
+export default function quickSort(array, left, right) {
+    if (array.length === 1) {
+        return;
+    } else {
+        const anchor = array[array.length - 1];
+        const anchorPoint = partition(array, left, right);
+        quickSort(array, left, anchorPoint - 1);
+        quickSort(array, anchorPoint + 1, right);
+    }
+}
+
+function partition(array, left, right): any {
+
+}
