06bdb96a266cd4c1acff622366cfb70be8c60969
Danila <fourlessthanthree@yahoo.com>
2017-03-28 23:23:46 +0300

    Hacking atom plugin

---
+++ test-atom-plugin/spec/test-atom-plugin-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TestAtomPluginView from '../lib/test-atom-plugin-view';
+
+describe('TestAtomPluginView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
