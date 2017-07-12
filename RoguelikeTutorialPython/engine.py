b91d44cf42169df7f01af0cf7d1021930242e651
Danila <fourlessthanthree@yahoo.com>
2017-07-12 23:31:25 +0300

    Part 3 "dungeon" progress

--- RoguelikeTutorialPython/engine.py
+++ RoguelikeTutorialPython/engine.py
@@ -1,18 +1,22 @@
-import libtcodpy as libtcod
 import sys
-
-sys.dont_write_bytecode = True
+import libtcodpy as libtcod
 
 from entity import Entity
 from input_handlers import handle_keys
 from map_objects.game_map import GameMap
 from render_functions import clear_all, render_all
 
+sys.dont_write_bytecode = True
+
 def main():
     screen_width = 80
     screen_height = 50
     map_width = 80
     map_height = 45
+
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
@@ -30,6 +34,7 @@ def main():
     con = libtcod.console_new(screen_width, screen_height)
 
     game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
@@ -62,3 +67,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
