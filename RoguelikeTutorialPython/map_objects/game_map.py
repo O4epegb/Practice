4b78e8010673c673c5059778c04580c5ea11413f
Danila <fourlessthanthree@yahoo.com>
2017-08-24 01:34:31 +0300

    Implement more combat

--- RoguelikeTutorialPython/map_objects/game_map.py
+++ RoguelikeTutorialPython/map_objects/game_map.py
@@ -9,6 +9,8 @@ from entity import Entity
 from map_objects.tile import Tile
 from map_objects.rectangle import Rect
 
+from render_functions import RenderOrder
+
 class GameMap:
     def __init__(self, width, height):
         self.width = width
@@ -104,11 +106,13 @@ class GameMap:
                 if randint(0, 100) < 80:
                     fighter_component = Fighter(hp=10, defense=0, power=3)
                     ai_component = BasicMonster()
-                    monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True, fighter=fighter_component, ai=ai_component)
+                    monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True,
+                                     render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component)
                 else:
                     fighter_component = Fighter(hp=16, defense=1, power=4)
                     ai_component = BasicMonster()
-                    monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks=True, fighter=fighter_component, ai=ai_component)
+                    monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks=True, fighter=fighter_component,
+                                     render_order=RenderOrder.ACTOR, ai=ai_component)
 
                 entities.append(monster)
 
