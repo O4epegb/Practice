e56e60e25f165e91af3642a4edc4e94b4e2e8538
Danila <fourlessthanthree@yahoo.com>
2017-09-03 22:30:31 +0300

    Part 8: inventory and items

--- RoguelikeTutorialPython/map_objects/game_map.py
+++ RoguelikeTutorialPython/map_objects/game_map.py
@@ -3,8 +3,11 @@ from random import randint
 
 from components.ai import BasicMonster
 from components.fighter import Fighter
+from components.item import Item
 
 from entity import Entity
+
+from item_functions import heal
 
 from map_objects.tile import Tile
 from map_objects.rectangle import Rect
@@ -122,7 +125,9 @@ class GameMap:
             y = randint(room.y1 + 1, room.y2 - 1)
 
             if not any([entity for entity in entities if entity.x == x and entity.y == y]):
-                item = Entity(x, y, '!', libtcod.violet, 'Healing Potion', render_order=RenderOrder.ITEM)
+                item_component = Item(use_function=heal, amount=4)
+                item = Entity(x, y, '!', libtcod.violet, 'Healing Potion', render_order=RenderOrder.ITEM,
+                              item=item_component)
 
                 entities.append(item)
 
